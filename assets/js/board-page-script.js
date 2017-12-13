9f4942b8df852525aefca2f323c6f11165a1ae6e
Ivan Milicevic <ivmilicevic1@gmail.com>
2017-12-13 09:55:02 +0100

    Code refactoring, input field now always spawns at bottom of list

--- assets/js/board-page-script.js
+++ assets/js/board-page-script.js
@@ -24,22 +24,27 @@ var liInputDiv = document.querySelector("#liInputDiv");
 var list0 = document.querySelector("#list0");
 document.querySelector("#confirmationSuccessBtn").addEventListener("click", function() {
     let liText = listItemTextInput.value;
-    console.log(liText);
-
-    let node = document.createElement("a");
-    node.className = "list-group-item list-group-item-action flex-column align-items-start";
-    node.innerHTML = `
-    <div class="d-flex flex-row-reverse w-100 justify-content-between align-items-baseline">
-    <button type="button" class="btn btn-outline-secondary btn-sm">
-        <i class="fa fa-wrench"></i>
-    </button>
-    </div>
-    <p class="mb-1">${liText}</p>
-    `;
-    console.log(node);
-    list0.appendChild(node);
+    // list0.appendChild(createListItem(liText));
+    // Inserts new list item before input field, so div with textarea always spawns at bottom of list
+    list0.insertBefore(createListItem(liText), document.querySelector("#liInputDiv"));
     addListItemDiv.style.display = "block";
     addListItemConfirmation.style.display = "none";
     liInputDiv.style.display = "none";
     listItemTextInput.value = "";
 });
+
+function createListItem(content) {
+    // Creates new list item, ready for inserting into list
+    // "content" is desired value of list item
+    let node = document.createElement("a");
+    node.className = "list-group-item list-group-item-action flex-column align-items-start";
+    node.innerHTML = `
+        <div class="d-flex flex-row-reverse w-100 justify-content-between align-items-baseline">
+        <button type="button" class="btn btn-outline-secondary btn-sm">
+            <i class="fa fa-wrench"></i>
+        </button>
+        </div>
+        <p class="mb-1">${content}</p>
+    `;
+    return node;
+}
