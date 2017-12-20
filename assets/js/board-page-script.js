5a53f0ea575a997ecc4b08a1a9d69c1e2eafc82c
Ivan Milicevic <ivmilicevic1@gmail.com>
2017-12-20 09:21:10 +0100

    Code cleanup, started moving code to one global function

--- assets/js/board-page-script.js
+++ assets/js/board-page-script.js
@@ -1,37 +1,6 @@
-// document.querySelector("#addListItemBtn").addEventListener("click", function () {
-//     // alert("Add list item...");
-//     addListItemDiv.style.display = "none";
-//     addListItemConfirmation.style.display = "block";
-//     liInputDiv.style.display = "block";
-// });
+var app = new function(){
 
-// document.querySelector("#lists-row").addEventListener("click", removeListItem);
-
-
-
-// var addListItemDiv = document.querySelector("#addListItemDiv");
-// var addListItemConfirmation = document.querySelector("#addListItemConfirmation");
-// var confirmationCancelBtn = document.querySelector("#confirmationCancelBtn").addEventListener("click", function () {
-//     addListItemDiv.style.display = "block";
-//     addListItemConfirmation.style.display = "none";
-//     liInputDiv.style.display = "none";
-// });
-
-// var listItemTextInput = document.querySelector("#listItemTextInput");
-
-// var liInputDiv = document.querySelector("#liInputDiv");
-
-// var list0 = document.querySelector("#list0");
-// document.querySelector("#confirmationSuccessBtn").addEventListener("click", function () {
-//     let liText = listItemTextInput.value;
-//     // list0.appendChild(createListItem(liText));
-//     // Inserts new list item before input field, so div with textarea always spawns at bottom of list
-//     list0.insertBefore(createListItem(liText), document.querySelector("#liInputDiv"));
-//     addListItemDiv.style.display = "block";
-//     addListItemConfirmation.style.display = "none";
-//     liInputDiv.style.display = "none";
-//     listItemTextInput.value = "";
-// });
+}
 
 function createListItem(content) {
     // Creates new list item, ready for inserting into list
@@ -175,5 +144,3 @@ function displayBoardName(name){
     let boardName = document.getElementById("board-name");
     boardName.innerHTML = name;
 }
-
-// console.log(createList("exampleName", 5))
